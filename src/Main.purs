ad91ade2e4d5b99e61367d0ac85b919645bae7d0
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2016-09-19 14:11:25 +0100

    Main for experiments

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,20 @@
 module Main where
 
 import Prelude
+import PouchDB (info, pouchDB, PouchDB, PouchDoc)
+import Control.Monad.Aff
+import Control.Monad.Aff.Class (liftAff)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Aff.Console (CONSOLE, log)
+import Control.Monad.Eff.Exception (EXCEPTION, throwException, error, message, try)
+import Unsafe.Coerce (unsafeCoerce)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "Hello sailor!"
+info' :: forall e. PouchDB -> Aff e PouchDoc
+info' db = makeAff (\e s -> info db e s)
+
+main = launchAff do
+  let db = pouchDB "thedatabase"
+  i <- info' db
+  let i' = unsafeCoerce i
+  log i'
+
