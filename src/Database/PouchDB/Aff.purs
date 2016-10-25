d0558dc0e5b672c147efbe8517f1cbabfca96941
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2016-10-25 10:34:16 +0100

    Destroy, put, get

--- src/Database/PouchDB/Aff.purs
+++ src/Database/PouchDB/Aff.purs
@@ -8,7 +8,7 @@ import Data.Foreign (Foreign, unsafeFromForeign, writeObject)
 
 foreign import data POUCHDB :: !
 
--- type Document = Foreign
+type Document = Foreign
 
 newtype Id = Id String
 newtype Rev = Rev String
@@ -25,5 +25,19 @@ info db = makeAff (\e s -> FFI.info db e (s <<< foreignToInfo))
 pouchDB :: String -> PouchDB
 pouchDB name = FFI.pouchDB name empty
 
+--| Deletes the database.
+--|
+--| You should not try to use the handle again.
+--| If you need a new database with the same name, call `pouchDB` again.
 destroy :: forall e. PouchDB -> Aff (pouchdb :: POUCHDB | e) {ok :: Boolean}
 destroy db = makeAff (\e s -> FFI.destroy db empty e (s <<< unsafeFromForeign))
+
+--| Create or update document
+--|
+--| The document needs to have a field `_id`.
+--| For updating an existing document, it also has to have the current `_rev`.
+put :: forall e. PouchDB -> Document -> Aff (pouchdb :: POUCHDB | e) {ok :: Boolean, id :: Id , rev :: Rev}
+put db doc = makeAff (\e s -> FFI.put db doc empty e (s <<< unsafeFromForeign))
+
+get :: forall e. PouchDB -> Id -> Aff (pouchdb :: POUCHDB | e) Document
+get db (Id id) = makeAff (\e s -> FFI.get db id empty e s)
--- test/Main.purs
+++ test/Main.purs
@@ -3,12 +3,16 @@ module Test.Main where
 import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE)
-import Database.PouchDB.Aff (destroy, info, pouchDB, POUCHDB)
-import Prelude (bind, Unit)
-import Test.Unit (suite, test)
+import Control.Monad.Aff (attempt)
+import Database.PouchDB.Aff (POUCHDB, Id, destroy, get, info, pouchDB, put)
+import Prelude (bind, Unit, (==), ($))
+import Test.Unit (suite, test, success, failure)
 import Test.Unit.Assert as Assert
 import Test.Unit.Console (TESTOUTPUT)
 import Test.Unit.Main (runTest)
+import Data.Foreign (toForeign)
+import Unsafe.Coerce (unsafeCoerce)
+import Data.Either (Either (Left, Right))
 
 main :: forall e. Eff (pouchdb :: POUCHDB, console :: CONSOLE, testOutput :: TESTOUTPUT, avar :: AVAR | e) Unit
 main = runTest do
@@ -17,7 +21,30 @@ main = runTest do
     test "local database name" do
       i <- info db
       Assert.equal i.db_name "localdatabase"
+  suite "simple read write" do
+    test "write doc response" do
+      r <- put db (toForeign {_id:"mydoc", title:"test"})
+      Assert.equal true r.ok
+      Assert.equal (unsafeCoerce r.id) "mydoc"
+      Assert.assertFalse "rev empty" ((unsafeCoerce r.rev) == "")
+    test "read written doc" do
+      d <- get db (unsafeCoerce "mydoc")
+      let doc = unsafeCoerce d :: {title :: String, _id :: Id}
+      Assert.equal "test" doc.title
+      Assert.equal "mydoc" (unsafeCoerce doc._id)
+      Assert.assert "ok" true
+  suite "write conflict" do
+    test "write doc twice conflicts" do
+      _ <- put db (toForeign {_id:"doublewrite"})
+      a <- attempt $ put db (toForeign {_id:"doublewrite"})
+      case a of
+        -- TODO parse errors
+        Left e -> Assert.equal ((unsafeCoerce e).name) "conflict"
+        Right r -> failure "attempting to write a document twice should fail"
   suite "destroy" do
     test "returns ok:true" do
       r <- destroy (pouchDB "create-and-destroy")
       Assert.assert "not okay" r.ok
+    test "clean up localdatabase" do
+      _ <- destroy db
+      success
