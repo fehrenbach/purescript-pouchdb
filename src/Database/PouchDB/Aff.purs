40abac8ca493f5e724fd73bfcdb93ace0a361045
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2016-10-27 17:59:57 +0100

    Better types and stuff

--- src/Database/PouchDB/Aff.purs
+++ src/Database/PouchDB/Aff.purs
@@ -1,26 +1,64 @@
 module Database.PouchDB.Aff where
 
 import Prelude
+import Data.Newtype (class Newtype)
 import Database.PouchDB.FFI (PouchDB)
 import Database.PouchDB.FFI as FFI
 import Control.Monad.Aff (Aff, makeAff)
-import Data.Foreign (Foreign, unsafeFromForeign, writeObject)
+import Data.Foreign (Foreign, unsafeFromForeign, writeObject, toForeign)
+import Data.Foreign.Class (class IsForeign, class AsForeign, read, readProp, write)
+import Control.Monad.Except (runExcept)
+import Data.Either (Either (..))
+import Unsafe.Coerce (unsafeCoerce)
+import Control.Monad.Eff.Console (log)
+import Control.Monad.Eff.Unsafe (unsafePerformEff)
+import Control.Monad.Eff.Exception (error)
 
 foreign import data POUCHDB :: !
 
-type Document = Foreign
+-- TODO remove (or move into its own library or something)
+dbg :: forall a. String -> a -> a
+dbg s a = unsafePerformEff $ do
+  log s
+  log (unsafeCoerce a)
+  pure a
+
+data Document d =
+  Document Id Rev d
+
+instance isForeignDocument :: IsForeign d => IsForeign (Document d) where
+  read value = do
+    id <- readProp "_id" value
+    rev <- readProp "_rev" value
+    d <- read value
+    pure $ Document id rev d
+
+instance asForeignDocument :: AsForeign d => AsForeign (Document d) where
+  write (Document i r d) =
+    -- This is a bit of a hack, because we need to serialize the
+    -- payload to an object, then on that object set _id and _rev.
+    toForeign $ (unsafeCoerce (write d)) {_id = write i, _rev = write r}
+
 
 newtype Id = Id String
+
+derive instance newtypeId :: Newtype Id _
+derive newtype instance asForeignId :: AsForeign Id
+derive newtype instance isForeignId :: IsForeign Id
+
+
 newtype Rev = Rev String
+
+derive instance newtypeRev :: Newtype Rev _
+derive newtype instance asForeignRev :: AsForeign Rev
+derive newtype instance isForeignRev :: IsForeign Rev
+
 
 empty :: Foreign
 empty = writeObject []
 
-foreignToInfo :: Foreign -> {db_name :: String, doc_count :: Number, update_seq :: Number}
-foreignToInfo f = unsafeFromForeign f
-
-info :: forall e. PouchDB -> Aff (pouchdb :: POUCHDB | e) {db_name :: String, doc_count :: Number, update_seq :: Number}
-info db = makeAff (\e s -> FFI.info db e (s <<< foreignToInfo))
+info :: forall e. PouchDB -> Aff (pouchdb :: POUCHDB | e) {db_name :: String, doc_count :: Int, update_seq :: Int}
+info db = makeAff (\e s -> FFI.info db e (s <<< unsafeFromForeign))
 
 pouchDB :: String -> PouchDB
 pouchDB name = FFI.pouchDB name empty
@@ -32,12 +70,22 @@ pouchDB name = FFI.pouchDB name empty
 destroy :: forall e. PouchDB -> Aff (pouchdb :: POUCHDB | e) {ok :: Boolean}
 destroy db = makeAff (\e s -> FFI.destroy db empty e (s <<< unsafeFromForeign))
 
---| Create or update document
---|
---| The document needs to have a field `_id`.
---| For updating an existing document, it also has to have the current `_rev`.
-put :: forall e. PouchDB -> Document -> Aff (pouchdb :: POUCHDB | e) {ok :: Boolean, id :: Id , rev :: Rev}
-put db doc = makeAff (\e s -> FFI.put db doc empty e (s <<< unsafeFromForeign))
+-- TODO change return type to Document
+create :: forall d e. AsForeign d => PouchDB -> Id -> d -> Aff (pouchdb :: POUCHDB | e) Foreign
+create db (Id id) doc = makeAff (\kE kS ->
+  FFI.put db docWithId empty kE kS)
+    where docWithId = toForeign $ (unsafeCoerce (write doc)) {_id = id}
 
-get :: forall e. PouchDB -> Id -> Aff (pouchdb :: POUCHDB | e) Document
-get db (Id id) = makeAff (\e s -> FFI.get db id empty e s)
+-- TODO change return type to Aff .. (Document d) with updated id rev
+update :: forall d e. AsForeign d => PouchDB -> (Document d) -> Aff (pouchdb :: POUCHDB | e) {ok :: Boolean, id :: Id, rev :: Rev}
+update db doc = makeAff (\kE kS ->
+  FFI.put db (write doc) empty kE (kS <<< unsafeFromForeign))  
+
+get :: forall d e. IsForeign d => PouchDB -> Id -> Aff (pouchdb :: POUCHDB | e) (Document d)
+get db (Id id) = makeAff (\kE kS ->
+  FFI.get db id empty
+          (\e -> kE e)
+          (\r -> case runExcept (read r) of
+             -- TODO figure out whether we can properly attach data to an error
+             Left parseError -> kE (error $ "parse error: " <> show parseError)
+             Right d -> kS d))
--- test/Main.purs
+++ test/Main.purs
@@ -3,16 +3,39 @@ module Test.Main where
 import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE)
-import Control.Monad.Aff (attempt)
-import Database.PouchDB.Aff (POUCHDB, Id, destroy, get, info, pouchDB, put)
-import Prelude (bind, Unit, (==), ($))
-import Test.Unit (suite, test, success, failure)
+import Control.Monad.Aff.Console (log)
+import Database.PouchDB.Aff (POUCHDB, Document (..), destroy, get, info, pouchDB, create)
+import Prelude
+import Test.Unit (suite, test, success)
 import Test.Unit.Assert as Assert
 import Test.Unit.Console (TESTOUTPUT)
 import Test.Unit.Main (runTest)
-import Data.Foreign (toForeign)
 import Unsafe.Coerce (unsafeCoerce)
-import Data.Either (Either (Left, Right))
+import Data.Foreign (ForeignError(ForeignError), fail, writeObject)
+import Data.Foreign.Class (class AsForeign, class IsForeign, readProp, (.=))
+
+data Movie = Movie { title :: String,
+                     year :: Int,
+                     actors :: Array String }
+
+instance asForeignMovie :: AsForeign Movie where
+  -- TODO if this works, clean it up a bit
+  write (Movie {title, year, actors}) =
+    writeObject [ "type" .= "movie"
+                , "title" .= title
+                , "year" .= year
+                , "actors" .= actors ]
+
+instance isForeignMovie :: IsForeign Movie where
+  read value = do
+    typ :: String <- readProp "type" value
+    title <- readProp "title" value
+    year <- readProp "year" value
+    actors <- readProp "actors" value
+    -- TODO should probably do that first
+    if (typ /= "movie")
+      then fail (ForeignError "Try to parse doc with type != 'movie' as Movie")
+      else pure $ Movie {title, year, actors}
 
 main :: forall e. Eff (pouchdb :: POUCHDB, console :: CONSOLE, testOutput :: TESTOUTPUT, avar :: AVAR | e) Unit
 main = runTest do
@@ -23,24 +46,23 @@ main = runTest do
       Assert.equal i.db_name "localdatabase"
   suite "simple read write" do
     test "write doc response" do
-      r <- put db (toForeign {_id:"mydoc", title:"test"})
-      Assert.equal true r.ok
-      Assert.equal (unsafeCoerce r.id) "mydoc"
-      Assert.assertFalse "rev empty" ((unsafeCoerce r.rev) == "")
+      r <- create db (unsafeCoerce "juno") (Movie { title: "Juno", year: 2007, actors: ["Ellen Page", "Michael Cera"]})
+      log (unsafeCoerce r)
+      -- Assert.equal true r.ok
+      -- Assert.equal (unsafeCoerce r.id) "mydoc"
+      -- Assert.assertFalse "rev empty" ((unsafeCoerce r.rev) == "")
     test "read written doc" do
-      d <- get db (unsafeCoerce "mydoc")
-      let doc = unsafeCoerce d :: {title :: String, _id :: Id}
-      Assert.equal "test" doc.title
-      Assert.equal "mydoc" (unsafeCoerce doc._id)
-      Assert.assert "ok" true
-  suite "write conflict" do
-    test "write doc twice conflicts" do
-      _ <- put db (toForeign {_id:"doublewrite"})
-      a <- attempt $ put db (toForeign {_id:"doublewrite"})
-      case a of
-        -- TODO parse errors
-        Left e -> Assert.equal ((unsafeCoerce e).name) "conflict"
-        Right r -> failure "attempting to write a document twice should fail"
+      doc@(Document id rev (m :: Movie)) <- get db (unsafeCoerce "juno")
+      log (unsafeCoerce doc)
+      log (unsafeCoerce m)
+  -- suite "write conflict" do
+  --   test "write doc twice conflicts" do
+  --     _ <- put db (toForeign {_id:"doublewrite"})
+  --     a <- attempt $ put db (toForeign {_id:"doublewrite"})
+  --     case a of
+  --       -- TODO parse errors
+  --       Left e -> Assert.equal ((unsafeCoerce e).name) "conflict"
+  --       Right r -> failure "attempting to write a document twice should fail"
   suite "destroy" do
     test "returns ok:true" do
       r <- destroy (pouchDB "create-and-destroy")
