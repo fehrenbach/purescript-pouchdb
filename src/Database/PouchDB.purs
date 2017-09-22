39c77d097d09f8276c61208bdcdea2d99fed5cff
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2017-09-22 17:10:47 +0100

    Newtyped record based version
    This is a bit more machinery-heavy but allows for reference cycles.

--- src/Database/PouchDB.purs
+++ src/Database/PouchDB.purs
@@ -9,9 +9,9 @@ import Control.Monad.Eff.Exception (Error, error)
 import Control.Monad.Except (runExcept)
 import Data.Array (zipWith)
 import Data.Either (Either(..), either)
-import Data.Foreign (Foreign, F, toForeign)
+import Data.Foreign (F, Foreign, toForeign)
 import Data.Generic (class Generic)
-import Data.Newtype (class Newtype, wrap)
+import Data.Newtype (class Newtype, unwrap, wrap)
 import Data.Record (insert, set)
 import Data.Traversable (sequence)
 import Database.PouchDB.FFI (POUCHDB, PouchDB)
@@ -20,12 +20,8 @@ import Simple.JSON (class ReadForeign, class WriteForeign, read, write)
 import Type.Prelude (class RowLacks, SProxy(..))
 import Unsafe.Coerce (unsafeCoerce)
 
---| A document with contents `d` is a record with `_id` and `_rev`
---| fields of type `Id d` and `Rev d` respectively.
-type Document (d :: # Type) = { _id :: Id d, _rev :: Rev d | d }
 
-
-newtype Id (d :: # Type) = Id String
+newtype Id (d :: Type) = Id String
 
 derive instance newtypeId :: Newtype (Id d) _
 derive instance eqId :: Eq (Id d)
@@ -36,7 +32,7 @@ derive newtype instance readForeignId :: ReadForeign (Id d)
 derive newtype instance writeForeignId :: WriteForeign (Id d)
 
 
-newtype Rev (d :: # Type) = Rev String
+newtype Rev (d :: Type) = Rev String
 
 derive instance newtypeRev :: Newtype (Rev d) _
 derive instance eqRev :: Eq (Rev d)
@@ -45,6 +41,15 @@ derive instance genericRev :: Generic (Rev d)
 derive newtype instance showRev :: Show (Rev d)
 derive newtype instance readForeignRev :: ReadForeign (Rev d)
 derive newtype instance writeForeignRev :: WriteForeign (Rev d)
+
+
+--| Fetch a document by `_id`.
+getDoc :: forall doc dat e.
+  ReadForeign doc =>
+  Newtype doc { _id :: Id doc, _rev :: Rev doc | dat } =>
+  PouchDB -> Id doc -> Aff (pouchdb :: POUCHDB | e) doc
+getDoc db (Id id) = makeAff (\kE kS ->
+  FFI.get db id (toForeign {}) kE (\r -> either (kE <<< error <<< show) kS (runExcept (read r))))
 
 
 pouchDB :: forall e. String -> Aff (pouchdb :: POUCHDB | e) PouchDB
@@ -68,54 +73,54 @@ destroy :: forall e. PouchDB -> Aff (pouchdb :: POUCHDB | e) Unit
 destroy db = makeAff (\kE kS -> FFI.destroy db (toForeign {}) kE (\_ -> kS unit))
 
 
---| Fetch a document by `_id`.
-getDoc :: forall d e.
-  ReadForeign (Document d) =>
-  PouchDB -> Id d -> Aff (pouchdb :: POUCHDB | e) (Document d)
-getDoc db (Id id) = makeAff (\kE kS ->
-  FFI.get db id (toForeign {}) kE (\r -> either (kE <<< error <<< show) kS (runExcept (read r))))
-
-
 --| Save the given proto-document as a new document with the given id.
 --|
---| The proto-document is a record `{|r}` whose row `r` has to mach the row in `Id r`.
---| It may not contain `_id` and `_rev` fields.
+--| The document doc needs to be a newtype over a with `_id`, `_rev`
+--| and some data. The proto-document is just the data record, without
+--| `_id` and `_rev` fields.
 --|
 --| This is the recommended way to generate documents, rather than relying on
 --| PouchDB to make up a random document ID for you.
-createDoc :: forall e r.
-  WriteForeign { _id :: Id r | r} => RowLacks "_id" r => RowLacks "_rev" ( _id :: Id r | r ) => 
-  PouchDB -> Id r -> {| r } -> Aff (pouchdb :: POUCHDB | e) (Document r)
+createDoc :: forall doc dat e.
+  WriteForeign { _id :: Id doc | dat } =>
+  RowLacks "_id" dat =>
+  RowLacks "_rev" ( _id :: Id doc | dat ) =>
+  Newtype doc { _id :: Id doc, _rev :: Rev doc | dat } =>
+  PouchDB -> Id doc -> {| dat} -> Aff (pouchdb :: POUCHDB | e) doc
 createDoc db i protoDoc = makeAff (\kE kS ->
-  FFI.put db (write doc) (toForeign {}) kE (\r -> kS (insert (SProxy :: SProxy "_rev") (Rev r.rev) doc)))
+  FFI.put db (write doc) (toForeign {}) kE
+  (\r -> kS (wrap (insert (SProxy :: SProxy "_rev") (Rev r.rev) doc))))
   where
-    doc :: { _id :: Id r | r }
+    doc :: { _id :: Id doc | dat }
     doc = insert (SProxy :: SProxy "_id") i protoDoc
-
 
 --| Write a (modified) document back to the database.
 --|
 --| On success, returns the document itself with updated `_rev`.
-saveDoc :: forall d e.
-  WriteForeign (Document d) =>
-  PouchDB -> Document d -> Aff (pouchdb :: POUCHDB | e) (Document d)
+saveDoc :: forall doc dat e.
+  WriteForeign doc =>
+  Newtype doc { _id :: Id doc, _rev :: Rev doc | dat } =>
+  PouchDB -> doc -> Aff (pouchdb :: POUCHDB | e) doc
 saveDoc db doc = makeAff (\kE kS ->
-  FFI.put db (write doc) (toForeign {}) kE (\r -> kS (set (SProxy :: SProxy "_rev") (wrap r.rev) doc)))
+  FFI.put db (write doc) (toForeign {}) kE
+  (\r -> kS (wrap (unwrap doc) {_rev = Rev r.rev})))
 
 
 --| Delete a document from the database
 --|
 --| Unlike PouchDB's `remove`, this does not clear all fields, but merely sets `_deleted: true`.
-deleteDoc :: forall d e.
-  WriteForeign { _deleted :: Boolean, _id :: Id d, _rev :: Rev d | d } =>
-  RowLacks "_deleted" (_id :: Id d, _rev :: Rev d | d) =>
-  PouchDB -> Document d -> Aff (pouchdb :: POUCHDB | e) (Document d)
+deleteDoc :: forall doc dat e.
+  WriteForeign { _deleted :: Boolean, _id :: Id doc, _rev :: Rev doc | dat } =>
+  RowLacks "_deleted" (_id :: Id doc, _rev :: Rev doc | dat) =>
+  Newtype doc { _id :: Id doc, _rev :: Rev doc | dat } =>
+  PouchDB -> doc -> Aff (pouchdb :: POUCHDB | e) doc
 deleteDoc db doc = makeAff (\kE kS ->
-  FFI.put db (write deleted) (toForeign {}) kE (\r -> kS (set (SProxy :: SProxy "_rev") (wrap r.rev) doc)))
+  FFI.put db (write deleted) (toForeign {}) kE
+  (\r -> kS (wrap (set (SProxy :: SProxy "_rev") (wrap r.rev) udoc))))
   where
-    deleted :: { _deleted :: Boolean, _id :: Id d, _rev :: Rev d | d }
-    deleted = insert (SProxy :: SProxy "_deleted") true doc
-
+    udoc = unwrap doc
+    deleted :: { _deleted :: Boolean, _id :: Id doc, _rev :: Rev doc | dat }
+    deleted = insert (SProxy :: SProxy "_deleted") true udoc
 
 --| Fetch multiple documents at once.
 --|
@@ -124,11 +129,12 @@ deleteDoc db doc = makeAff (\kE kS ->
 --| This uses PouchDB's `allDocs` under the hood. It should not be
 --| confused with PouchDB's actual `bulkGet` which is used mainly
 --| internally for replication.
-bulkGet :: forall d e.
-  ReadForeign (Document d) =>
-  PouchDB -> Array (Id d) -> Aff (pouchdb :: POUCHDB | e) (Array (Document d))
+bulkGet :: forall doc dat e.
+  ReadForeign doc =>
+  Newtype doc { _id :: Id doc, _rev :: Rev doc | dat } =>
+  PouchDB -> Array (Id doc) -> Aff (pouchdb :: POUCHDB | e) (Array doc)
 bulkGet db ids = makeAff (\kE kS ->
-  FFI.allDocs db (write {keys: ids, include_docs: true}) kE (\r -> either (kE <<< error <<< show) kS (runExcept (read (aFtoF r.rows)))))
+  FFI.allDocs db (write {keys: ids, include_docs: true}) kE (\r -> either (kE <<< error <<< show) kS (runExcept (read (toForeign r.rows)))))
 
 
 --| Write multiple (modified) documents back to the database.
@@ -137,15 +143,17 @@ bulkGet db ids = makeAff (\kE kS ->
 --| This function will throw (only) the first error, if any.
 --|
 --| As far as I can tell, PouchDB will increase the revision number even if you write the same exact document that's already there. You might want to avoid that.
-bulkSave :: forall d e.
-  ReadForeign (Document d) => WriteForeign (Document d) =>
-  PouchDB -> Array (Document d) -> Aff (pouchdb :: POUCHDB | e) (Array (Document d))
+bulkSave :: forall dat doc e.
+  ReadForeign doc =>
+  WriteForeign doc =>
+  Newtype doc { _id :: Id doc, _rev :: Rev doc | dat } =>
+  PouchDB -> Array doc -> Aff (pouchdb :: POUCHDB | e) (Array doc)
 bulkSave db docs = makeAff (\kE kS ->
   FFI.bulkDocs db (map write docs) (toForeign {}) kE (\r -> either kE kS (sequence (zipWith decodeRow docs r))))
   where
-    decodeRow :: Document d -> Foreign -> Either Error (Document d)
+    decodeRow :: doc -> Foreign -> Either Error doc
     decodeRow doc f = case runExcept (read f :: F { ok :: Boolean, id :: String, rev :: String }) of
-      Right r -> Right (set (SProxy :: SProxy "_rev") (wrap r.rev) doc)
+      Right r -> Right (wrap (set (SProxy :: SProxy "_rev") (wrap r.rev) (unwrap doc)))
       -- TODO err here is a parse error
       -- we might want to read f as a proper PouchDB error instead and properly report that
       -- e.g. { status: 409, name: 'conflict', message: 'Document update conflict', error: true }
@@ -207,39 +215,30 @@ singleShotReplication source target = makeAff (\kE kS ->
   -- For now (no cancellation) return the actual value (for debugging purposes)
   FFI.replicateTo source target (toForeign {}) kE (\r -> kS (unsafeCoerce r)))
 
-type LinkedDocumentViewRow l d k v =
-  { doc :: Document d
-  , id :: Id l
-  , key :: k
-  , value :: v }
-
-type ViewRow d k v = LinkedDocumentViewRow d d k v
-
-aFtoF :: Array Foreign -> Foreign
-aFtoF = unsafeCoerce
 
 --| Simple keys query, no reduce, include docs.
 --|
 --| Throws the first parse error only (if any).
-viewKeysInclude :: forall d e k l v.
-  ReadForeign (LinkedDocumentViewRow l d k v) => WriteForeign k =>
-  PouchDB -> String -> Array k -> Aff (pouchdb :: POUCHDB | e) (Array (LinkedDocumentViewRow l d k v))
+viewKeysInclude :: forall l doc dat k v e.
+  ReadForeign { doc :: doc, id :: Id l, key :: k, value :: v } =>
+  WriteForeign k =>
+  Newtype doc { _id :: Id doc, _rev :: Rev doc | dat } =>
+  PouchDB -> String -> Array k -> Aff (pouchdb :: POUCHDB | e) (Array { doc :: doc, id :: Id l, key :: k, value :: v })
 viewKeysInclude db view keys = makeAff (\kE kS ->
   FFI.query db (write view) (write { keys, reduce: false, include_docs: true }) kE
-    (\r -> either (kE <<< error <<< show) kS (runExcept (read (aFtoF r.rows)))))
+    (\r -> either (kE <<< error <<< show) kS (runExcept (read (toForeign r.rows)))))
 
 
 --| Fetch all docs between startkey and endkey (inclusive)
 --|
 --| Consider using `docIdStartsWith` when looking for doc ids starting with some string.
-allDocsRange :: forall d e.
-  ReadForeign (Document d) =>
-  PouchDB ->
-  { startkey :: String, endkey :: String } ->
-  Aff (pouchdb :: POUCHDB | e) (Array (Document d))
+allDocsRange :: forall dat doc e.
+  ReadForeign doc =>
+  Newtype doc { _id :: Id doc, _rev :: Rev doc | dat } =>
+  PouchDB ->  { startkey :: String, endkey :: String } -> Aff (pouchdb :: POUCHDB | e) (Array doc)
 allDocsRange db {startkey, endkey} = makeAff (\kE kS ->
   FFI.allDocs db (write { startkey, endkey, include_docs: true }) kE
-    (\r -> either (kE <<< error <<< show) kS (runExcept (read (aFtoF r.rows)))))
+    (\r -> either (kE <<< error <<< show) kS (runExcept (read (toForeign r.rows)))))
 
 
 --| Construct a {startkey, endkey} record for range queries that should
--- test/Main.purs
+++ test/Main.purs
@@ -8,19 +8,30 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Eff.Exception (Error)
 import Data.Either (Either(..))
-import Data.Newtype (wrap)
-import Database.PouchDB (Document, Id, createDoc, deleteDoc, destroy, getDoc, info, pouchDB, saveDoc, singleShotReplication)
+import Data.Foreign.NullOrUndefined (NullOrUndefined(..))
+import Data.Maybe (Maybe(..))
+import Data.Newtype (class Newtype, unwrap, wrap)
+import Database.PouchDB (Id, Rev, createDoc, deleteDoc, destroy, getDoc, info, pouchDB, saveDoc, singleShotReplication)
 import Database.PouchDB.FFI (POUCHDB)
+import Simple.JSON (class ReadForeign, class WriteForeign)
 import Test.Unit (failure, suite, test)
 import Test.Unit.Assert as Assert
 import Test.Unit.Console (TESTOUTPUT)
 import Test.Unit.Main (runTest)
 import Unsafe.Coerce (unsafeCoerce)
 
-type Actor = (name :: String)
+newtype Actor = Actor { _id :: Id Actor, _rev :: Rev Actor, name :: String }
 
-type Movie = (title :: String, year :: Int, actors :: Array (Id Actor))
+derive instance newtypeActor :: Newtype Actor _
+derive newtype instance writeForeignActor :: WriteForeign Actor
+derive newtype instance readForeignActor :: ReadForeign Actor
 
+
+newtype Movie = Movie { _id :: Id Movie, _rev :: Rev Movie, title :: String, year :: Int, actors :: Array (Id Actor), sequel :: NullOrUndefined (Id Movie)}
+
+derive instance newtypeMovie :: Newtype Movie _
+derive newtype instance writeForeignMovie :: WriteForeign Movie
+derive newtype instance readForeignMovie :: ReadForeign Movie
 
 -- juno :: forall e. PouchDB -> Aff (pouchdb :: POUCHDB | e) (Document Movie)
 -- juno db = do ellenPage <- createDoc db (Id "ellen_page") { name: "Ellen Page" }
@@ -28,7 +39,7 @@ type Movie = (title :: String, year :: Int, actors :: Array (Id Actor))
 
 main :: forall e. Eff (pouchdb :: POUCHDB, console :: CONSOLE, testOutput :: TESTOUTPUT, avar :: AVAR | e) Unit
 main = runTest do
-  let juno = {title: "Juno", year: 2007, actors: ["Ellen Page", "Michael Cera"]}
+  let juno = {title: "Juno", year: 2007, actors: map wrap ["Ellen Page", "Michael Cera"], sequel: NullOrUndefined Nothing }
   suite "info" do
     test "local database name" do
       db <- pouchDB "localdatabase"
@@ -37,35 +48,36 @@ main = runTest do
   suite "simple read&write" do
     test "write doc response" do
       db <- pouchDB "localdatabase"
-      m <- createDoc db (wrap "juno") juno
+      Movie m <- createDoc db (wrap "juno") juno
       Assert.equal m._id (wrap "juno")
       Assert.equal m.title juno.title
+      Assert.equal m.sequel juno.sequel
     test "read written doc" do
       db <- pouchDB "localdatabase"
-      m :: Document Movie <- getDoc db (wrap "juno")
-      Assert.equal m._id (wrap "juno")
-      Assert.equal m.title juno.title
-      Assert.assertFalse "rev empty" (m._rev == wrap "")
+      m :: Movie <- getDoc db (wrap "juno")
+      Assert.equal (unwrap m)._id (wrap "juno")
+      Assert.equal (unwrap m).title juno.title
+      Assert.assertFalse "rev empty" ((unwrap m)._rev == wrap "")
     test "save doc" do
       db <- pouchDB "localdatabase"
-      d :: Document Movie <- getDoc db (wrap "juno")
+      Movie d <- getDoc db (wrap "juno")
       let d' = d {actors = map wrap ["Ellen Page", "Michael Cera", "Olivia Thirlby"]}
-      d'' <- saveDoc db d'
-      d''' :: Document Movie <- getDoc db (wrap "juno")
+      Movie d'' <- saveDoc db (Movie d')
+      Movie d''' <- getDoc db (wrap "juno")
       Assert.equal d''.actors d'''.actors
   suite "error cases" do
     test "create doc again conflicts" do
       db <- pouchDB "localdatabase"
-      a <- attempt $ createDoc db (wrap "juno") juno
+      a :: Either Error Movie <- attempt $ createDoc db (wrap "juno") juno
       case a of
         -- TODO provide a way to deal with (common) errors nicely
         Left e -> Assert.equal ((unsafeCoerce e).name) "conflict"
         Right r -> failure "attempting to write a document twice should fail"
     test "read deleted doc fails" do
       db <- pouchDB "localdatabase"
-      junoDoc :: Document Movie <- getDoc db (wrap "juno")
-      deletedJuno :: Document Movie <- deleteDoc db junoDoc
-      rereadJuno :: Either Error (Document Movie) <- attempt $ getDoc db (wrap "juno")
+      junoDoc :: Movie <- getDoc db (wrap "juno")
+      deletedJuno :: Movie <- deleteDoc db junoDoc
+      rereadJuno :: Either Error Movie <- attempt $ getDoc db (wrap "juno")
       case rereadJuno of
         Left e -> do
           Assert.equal "not_found" ((unsafeCoerce e).name)
@@ -73,7 +85,7 @@ main = runTest do
         Right r -> failure "fetching deleted document should fail"
     test "recreate deleted document succeeds" do
       db <- pouchDB "localdatabase"
-      m <- createDoc db (wrap "juno") juno
+      Movie m <- createDoc db (wrap "juno") juno
       Assert.equal m._id (wrap "juno")
   suite "single-shot replication" do
     test "local/local" do
